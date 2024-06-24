057cc3223737f50e08c7cb9185e5554e2eaf4edf
Luke McLean <lukecmclean@gmail.com>
2024-06-24 11:59:49 -0300

    adds basic structure and basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1 class='underline'>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex w-full h-lvh">
+    <div class="my-auto mx-auto w-2/4">
+        <h1 class="text-3xl text-center">Luke McLean</h1>
+        <h2 class="text-center text-slate-600">problem solver, researcher, software developer, educator</h2>
+        <hr class="block border-t border-slate-300">
+        <ul class="text-center">
+            <li>
+                <a href="about" class="">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href='projects' class="">
+                    Projects
+                </a>
+            </li>
+            <li>
+                <a href="timeline" class="">
+                    Timeline
+                </a>
+            </li>
+            <li>
+                <a href="links" class="">
+                    Links
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
