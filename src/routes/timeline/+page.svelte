3908b758c6447311c46b0062e77b5404b40dd92d
Luke McLean <lukecmclean@gmail.com>
2024-08-04 13:36:55 -0300

    changes timevine to timeline.

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,66 +1,99 @@
 <!-- App.svelte -->
 <script lang="ts">
     import Ribbon from './ribbon.svelte';
-</script>
-  
-<div class="absolute w-full h-[1000px] bg-slate-100">
-    <div class="absolute top-[36px] right-[68%] lg:right-[58%] text-lg lg:text-2xl font-bold">Education</div>
-    <div class="absolute top-[36px] left-[65%] lg:left-[55%] text-lg lg:text-2xl tracking-tight font-bold">Work Experience</div>
+    import Linepoints from './linepoints.svelte';
+    const timeHeights = 
+        [`top-[19%]`, `top-[39%]`, `top-[56%]`, `top-[75%]`,
+        `top-[15%]`, `top-[33%]`, `top-[50%]`, `top-[73%]`, `top-[90%]`];
+    const dotSides = [`-translate-x-[100%]`, `-translate-x-[100%]`, `-translate-x-[100%]`, `-translate-x-[100%]`, 
+        ``, ``, ``, ``, ``, 
+    ]
+    
+    // const dates = [2024, 2023, 2022, 2021, 2020, 2019, 2012]
+    // const dateHeights = [`top-[11%]`, `top-[25%]`, `top-[35%]`, `top-[45%]`,
+    //     `top-[55%]`, `top-[65%]`, `top-[85%]`]
 
+</script>
+
+
+
+
+<div class="absolute w-full h-[1000px] max-w-4xl left-2/4 -translate-x-2/4 bg-slate-100">
+    
     <div class="absolute left-2/4 -translate-x-2/4 w-[120px] sm:w-[200px] h-full">
-        <Ribbon />
+        <Linepoints />
     </div>
-    <div class="absolute max-w-xs text-center right-[60%] lg:right-[54%] top-[11%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2022-2024</p>
-        <p class="underline">Master's in Research Psychology</p>
-        <p class="text-xs lg:text-base">major focus on coding complex data analysis and machine learning</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+    
+    <!-- Education time points -->
+    <div class="absolute w-2/4 right-2/4 h-full tracking-tight">
+        <div class="absolute top-[2%] left-2/4 -translate-x-2/4 text-lg lg:text-2xl font-bold">Education</div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[0]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2022-2024</p>
+            <p class="underline">Master's in Research Psychology</p>
+            <p class="text-xs lg:text-base">major focus on coding complex data analysis and machine learning</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[1]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2022</p>
+            <p class="underline">Coding Courses</p>
+            <p class="text-xs lg:text-base">Python, Machine Learning, HTML/CSS, TypeScript, Vue, C#</p>
+            <p class="text-xs lg:text-base">-Codecademy-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[2]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2020-2021</p>
+            <p class="underline">Bachelor of Education</p>
+            <p class="text-xs lg:text-base">Math and Science</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
+    
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[3]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2017-2020</p>
+            <p class="underline">Bachelor of Science in Biopsychology (Hons)</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
     </div>
-    <div class="absolute max-w-xs text-center right-[60%] lg:right-[52%] top-[28%] md:top-[26%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2022</p>
-        <p class="underline">Coding Courses</p>
-        <p class="text-xs lg:text-base">Python, Machine Learning, HTML/CSS, TypeScript, Vue, C#</p>
-        <p class="text-xs lg:text-base">-Codecademy-</p>
+    
+    <!-- work timepoints -->
+    <div class="absolute w-2/4 left-2/4 h-full">
+        <div class="absolute top-[2%] left-2/4 -translate-x-2/4 text-lg lg:text-2xl tracking-tight w-full text-center font-bold">Work Experience</div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[4]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2023-2024</p>
+            <p class="underline">Research Assistant</p>
+            <p class="text-xs lg:text-base">Psychology Research and Data Analysis</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[5]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2022-2023</p>
+            <p class="underline">University Teaching Assistant</p>
+            <p class="text-xs lg:text-base">Statistics and Psychology</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[6]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2021-2022</p>
+            <p class="underline">High School Teacher</p>
+            <p class="text-xs lg:text-base">Physics, Chemistry, and Music</p>
+            <p class="text-xs lg:text-base">-St. Stephen High School-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[7]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2019</p>
+            <p class="underline">Research Assistant</p>
+            <p class="text-xs lg:text-base">Data Collection and Analysis</p>
+            <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+        </div>
+        <div class="absolute text-center w-full px-8 xl:px-8 left-2/4 -translate-x-2/4 -translate-y-2/4 {timeHeights[8]} text-sm lg:text-lg tracking-tighter md:tracking-normal">
+            <p class="font-bold text-green-900 font-serif tracking-widest">2012-Present</p>
+            <p class="underline">Piano Teacher</p>
+            <p class="text-xs lg:text-base">all ages, styles, and skill levels</p>
+            <p class="text-xs lg:text-base">-St. Andrews, NB-</p>
+        </div>
     </div>
-    <div class="absolute max-w-xs text-center right-[63%] lg:right-[53%] top-[46%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2020-2021</p>
-        <p class="underline">Bachelor of Education</p>
-        <p class="text-xs lg:text-base">Math and Science</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
+    {#each timeHeights as height, index }
+        <hr class="w-8 border-t-2 border-green-950 absolute left-2/4 {height} {dotSides[index]}">
+    {/each}
+    <!-- {#each dateHeights as height, index }
+    <div class="bg-gray-100 border border-black bg-opacity-90 absolute left-2/4 {height} -translate-x-2/4 -translate-y-2/4 p-1">
+        {dates[index]}
     </div>
-    <div class="absolute max-w-xs text-center right-[62%] lg:right-[52%] top-[59%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2017-2020</p>
-        <p class="underline">Bachelor of Science in Biopsychology (Hons)</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
-    </div>
-    <div class="absolute max-w-xs text-center left-[65%] lg:left-[54%] top-[9%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2023-2024</p>
-        <p class="underline">Research Assistant</p>
-        <p class="text-xs lg:text-base">Psychology Research and Data Analysis</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
-    </div>
-    <div class="absolute max-w-xs text-center left-[60%] lg:left-[53%] top-[23%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2022-2023</p>
-        <p class="underline">University Teaching Assistant</p>
-        <p class="text-xs lg:text-base">Statistics and Psychology</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
-    </div>
-    <div class="absolute max-w-xs text-center left-[64%] lg:left-[55%] top-[40%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2021-2022</p>
-        <p class="underline">High School Teaching</p>
-        <p class="text-xs lg:text-base">Physics, Chemistry, and Music</p>
-        <p class="text-xs lg:text-base">-St. Stephen High School-</p>
-    </div>
-    <div class="absolute max-w-xs text-center left-[60%] lg:left-[52%] top-[58%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2019</p>
-        <p class="underline">Research Assistant</p>
-        <p class="text-xs lg:text-base">Data Collection and Analysis</p>
-        <p class="text-xs lg:text-base">-University of New Brunswick-</p>
-    </div>
-    <div class="absolute max-w-xs text-center left-[63%] lg:left-[55%] top-[74%] lg:top-[76%] text-sm lg:text-lg tracking-tighter md:tracking-normal">
-        <p class="font-bold text-green-900 font-serif tracking-widest">2012-Present</p>
-        <p class="underline">Piano Lessons</p>
-        <p class="text-xs lg:text-base">all ages, styles, and skill levels</p>
-        <p class="text-xs lg:text-base">-St. Andrews, NB-</p>
-    </div>
+    {/each} -->
+
 </div>
---
+++ src/routes/timeline/linepoints.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+  
+    let canvas: HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D;
+  
+    function drawRibbon() {
+        const width = canvas.width;
+        const height = canvas.height;
+        ctx.clearRect(0, 0, width, height);
+
+        ctx.beginPath();
+        ctx.moveTo(width / 2, 0);
+        ctx.lineTo(width /2, height)
+  
+      ctx.strokeStyle = 'darkgreen'; // Tailwind 'blue-500'
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+  
+    onMount(() => {
+      canvas.width = canvas.clientWidth;
+      canvas.height = canvas.clientHeight;
+      ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+      drawRibbon();
+    });
+  </script>
+  
+  <div class="w-full h-full flex items-center justify-center">
+    <canvas bind:this={canvas} class="w-full h-full"></canvas>
+  </div>
