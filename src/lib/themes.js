c0259692610572c96632b724a2d8911c8367f138
Luke McLean <lukecmclean@gmail.com>
2024-09-24 18:01:26 -0300

    fixes themes bug

--- src/lib/themes.js
+++ src/lib/themes.js
@@ -4,7 +4,7 @@ export const themes = {
     "Negativity": ['bad', 'worse', 'sad', 'cry', 'alone', 'die', 'dies', 'died', 'broke', 'ill', 'kill', 'dead', 'death', 'devil', 'angry', 'mad', 'tear', 'tearing', 'tears', 'broken'],
     "Ego": ['i', 'me', 'my', 'mine', 'im'],
     "Oneness": ['all', 'together', 'one', 'world', 'universe', 'always', 'forever', 'ever', 'whole'],
-    "Masculine": ['man', 'boy', 'he', 'his', 'mr.', 'mister', 'men', 'boys'],
-    "Feminine": ['herself', 'her', 'she', 'she', 'girl', 'girls', 'girlfriend', 'mistress', 'baby', 'woman', 'women'],
+    "Masculine": ['man', 'boy', 'he', 'his', 'mr', 'mister', 'men', 'boys'],
+    "Feminine": ['herself', 'her', 'she', 'girl', 'girls', 'girlfriend', 'mistress', 'baby', 'woman', 'women'],
     "Travel": ['travelled', 'travelling', 'train', 'car', 'ride', 'road', 'roam', 'go', 'there', 'far', 'going', 'went', 'boat', 'sailed', 'sail', 'sailing', 'ridin', 'riding', 'fly']
 }
