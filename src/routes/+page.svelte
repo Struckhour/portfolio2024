282520e3b6baecd4bcfd37f53e3e5e0ce777fddf
Luke McLean <lukecmclean@gmail.com>
2024-10-01 18:34:02 -0300

    adds triangulation animation and fixes Beatles pic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,59 @@
     import mug from '$lib/mug.png';
     import spec from '$lib/heth_spec_purple.png';
     import beatlepic from '$lib/beatles peppers.png';
+    import markovDiagram from '$lib/markov-diagram.png';
+    import bird from '$lib/bird.png';
 	import { onDestroy, onMount } from "svelte";
+
+
+    function createCircle(leftPerc: number) {
+        const forestBoxDiv = document.getElementById('forestBox');
+        if (!forestBoxDiv) {
+                return;
+            }
+        const circle= document.createElement('div');
+        circle.style.position = 'absolute';
+        circle.style.width = '0%';
+        // circle.style.height = `0%`;
+        circle.style.aspectRatio = '1/1';
+        circle.style.top = `28%`;
+        const leftPercentage = 50.5;
+        circle.style.left = `${leftPercentage * ((100 - leftPerc * 2)/100) + leftPerc}%`;
+        circle.style.border = 'lightblue 10px solid';
+        circle.style.opacity = '60%';
+        circle.style.borderRadius = '100000px';
+        circle.style.transform = 'translate(-50%, -50%)';
+
+        forestBoxDiv.appendChild(circle);
+        return circle;
+    }
+
+    const micStats: number[][] = [[25, 4, -45], [25, -6, -138], [25, 0, -80], [5, -7, -110]]
+
+    function createMicDiv(leftPerc: number, micStat: number[]){
+        console.log('inside createMidDiv');
+        const forestBoxDiv = document.getElementById('forestBox');
+        if (!forestBoxDiv) {
+                return;
+            }
+        const micOuterDiv = document.createElement('div');
+        forestBoxDiv.appendChild(micOuterDiv);
+        micOuterDiv.style.position = 'absolute';
+        micOuterDiv.style.width = '100%';
+        // micOuterDiv.style.height = '10%'
+        micOuterDiv.style.top = `${micStat[0]}%`;
+        const leftPercentage = micStat[1];
+        micOuterDiv.style.left = `${leftPercentage * ((100 - leftPerc * 2)/100) + leftPerc}%`;
+        micOuterDiv.style.transform = `rotate(${micStat[2]}deg)`;
+        // micOuterDiv.style.border = 'red 2px solid';
+        
+        const micDiv= document.createElement('div');
+        micDiv.style.width = '0';
+        micDiv.style.opacity = '90%';
+        micDiv.style.border = '#b6ff96 2px dashed';
+        micOuterDiv.appendChild(micDiv);
+        return micDiv;
+    }
 
     let songBoxDivs: HTMLDivElement[] = [];
     let songBoxLocs = [[1, 40, 3, 40],
@@ -67,11 +119,18 @@
         specBoxDiv.appendChild(songBox);
     };
 
+
+
     let specInterval: ReturnType<typeof setInterval>;
     let scanLine: HTMLDivElement | undefined;
 
     let forestInterval: ReturnType<typeof setInterval>;
     let birdCircle: HTMLDivElement | undefined;
+    let micDivs: (HTMLDivElement | undefined)[];
+    let micDivA: HTMLDivElement | undefined;
+    let micDivB: HTMLDivElement | undefined;
+    let micDivC: HTMLDivElement | undefined;
+    let micDivD: HTMLDivElement | undefined;
 
     onDestroy(() => {
         clearInterval(specInterval);
@@ -79,42 +138,69 @@
 
     function restartForest() {
         clearInterval(forestInterval);
-        Divs.forEach((div) => div.remove());
-        if (scanLine) scanLine.remove();
-    	const specDiv = document.getElementById('specDiv');
-        if (!specDiv) {
+        if (birdCircle) birdCircle.remove();
+    	const forestDiv = document.getElementById('forestDiv');
+        if (!forestDiv) {
                 return;
             }
-        const specLoc = specDiv.offsetLeft
-        const specWidth = specDiv.offsetWidth;
-        console.log("specLoc: ", specLoc)
-        console.log("specWidth: ", specWidth);
-        const totalWidth = specLoc + specLoc + specWidth;
-        const leftPerc = specLoc/totalWidth * 100;
-        const rightEdge = 100 - leftPerc
-        let lineLoc = leftPerc;
-        
-        scanLine = createScanLine(leftPerc);
-        let songIndex = 0;
-        let allDone = false;
+        const forestLoc = forestDiv.offsetLeft
+        const forestWidth = forestDiv.offsetWidth;
+        if (micDivA) micDivA.remove();
+        if (micDivB) micDivB.remove();
+        if (micDivC) micDivC.remove();
+        if (micDivD) micDivD.remove();
+        const totalWidth = forestLoc + forestLoc + forestWidth;
+        const leftPerc = forestLoc/totalWidth * 100;
+        const rightEdge = 100 - leftPerc;
+        let circleSize = 0;
+        const biggestCircle = 200;
+        let micAWidth = 0;
+        let micBWidth = 0;
+        let micCWidth = 0;
+        let micDWidth = 0;
+        console.log('before push')
+        birdCircle = createCircle(leftPerc);
+        micDivA = createMicDiv(leftPerc, micStats[0]);
+        micDivB = createMicDiv(leftPerc, micStats[1]);
+        micDivC = createMicDiv(leftPerc, micStats[2]);
+        micDivD = createMicDiv(leftPerc, micStats[3]);
+        console.log('after push');
         forestInterval = setInterval(() => {
-            if (scanLine) {
-                if (lineLoc < rightEdge) {
-                    lineLoc += .2;
-                    if (lineLoc > (songBoxLocs[songIndex][0] * ((100 - leftPerc * 2)/100) + leftPerc) && !allDone) {
-                        createSongBox(leftPerc, songBoxLocs[songIndex]);
-                        if (songIndex < songBoxLocs.length -1) {songIndex++;}
-                        else {allDone = true;}
-                    }
+            if (birdCircle) {
+                if (circleSize < biggestCircle) {
+                    circleSize += 1;
+                    birdCircle.style.width = `${circleSize}%`;
+
+                    // birdCircle.style.height = `${circleSize}%`;
                 } else {
-                    lineLoc = leftPerc;
-                    songIndex = 0;
-                    allDone = false;
-                    songBoxDivs.forEach((div) => div.remove());
+                    circleSize = 0;
+                    micAWidth = 0;
+                    if(micDivA) micDivA.style.width = `${micAWidth}%`;
+                    micBWidth = 0;
+                    if(micDivB) micDivB.style.width = `${micBWidth}%`;
+                    micCWidth = 0;
+                    if(micDivC) micDivC.style.width = `${micCWidth}%`;
+                    micDWidth = 0;
+                    if(micDivD) micDivD.style.width = `${micDWidth}%`;
                 }
-                scanLine.style.left = `${lineLoc}%`  
+                if (circleSize > 90) {
+                    if (micAWidth < 42) micAWidth += 1;
+                    if (micDivA) micDivA.style.width = `${micAWidth}%`;
+                }
+                if (circleSize > 90) {
+                    if (micBWidth < 42) micBWidth += 1;
+                    if (micDivB) micDivB.style.width = `${micBWidth}%`;
+                }
+                if (circleSize > 100) {
+                    if (micCWidth < 45) micCWidth += 1;
+                    if (micDivC) micDivC.style.width = `${micCWidth}%`;
+                }
+                if (circleSize > 65) {
+                    if (micDWidth < 28) micDWidth += .5;
+                    if (micDivD) micDivD.style.width = `${micDWidth}%`;
+                }
             }
-        }, 30) 
+        }, 15) 
     }
 
     function restartSpec() {
@@ -154,17 +240,19 @@
                 }
                 scanLine.style.left = `${lineLoc}%`  
             }
-        }, 30) 
+        }, 15) 
     }
 
     onMount(()=>{
         window.addEventListener('resize', onResize);
         restartSpec();
+        restartForest();
         return () => window.removeEventListener('resize', onResize);
     });
 
     function onResize() {
         restartSpec();
+        restartForest();
     }
 
 
@@ -182,21 +270,23 @@
 </div> -->
 
 <!-- triangulation -->
-<div id="forestBox" class="relative bg-black">
+<div class="relative bg-black">
+    <div id="forestBox" class="relative bg-black overflow-hidden max-w-6xl m-auto">
 
-    <div class="absolute w-full text-5xl z-40 text-white bottom-4 lg:bottom-12 rounded-2xl bg-black bg-opacity-50 px-4 py-2">
-        <div class="text-left ml-[15%] text-xl md:text-2xl lg:text-5xl">
-            Automated Acoustic Triangulation
+        <div class="absolute w-full text-5xl z-40 text-white bottom-4 lg:bottom-12 rounded-2xl bg-black bg-opacity-50 px-4 py-2">
+            <div class="text-left ml-[5%] text-xl md:text-2xl lg:text-5xl">
+                Automated Acoustic Triangulation
+            </div>
         </div>
-    </div>
 
-    <img id="forestDiv" alt="a dense forest" src={forest} class="relative mx-auto w-full max-w-6xl object-scale-down"/>
+        <img id="forestDiv" alt="a forest" src={forest} class="relative mx-auto w-full max-w-6xl object-scale-down"/>
+    </div>
 </div>
 
 <div class="text-center text-2xl mt-4 mb-12">
-    <p class="max-w-xl mx-auto my-2">
+    <p class="max-w-2xl mx-auto my-2">
 
-        For my master's research, I developed an automated system for tracking birds while they sing in the forest.
+        For my master's research, I developed an automated system for tracking birds while they sing in the forest. I was able to demonstrate that neighbouring hermit thrushes interact through movement and song.
     </p>
     <button on:click={() => goto("/triangulation")} class="border-2 border-green-900 rounded-full px-4 bg-green-900 bg-opacity-20 hover:bg-opacity-60 transition-opacity hover:text-white text-lg sm:text-2xl">
         Learn more
@@ -257,7 +347,7 @@
             Beatles Lyrics Over Time
         </div>
     </div>
-    <img alt="a dense forest" src={beatlepic} class="relative w-full max-h-[600px] object-cover"/>
+    <img alt="a dense forest" src={beatlepic} class="relative w-full max-h-[600px] object-cover object-top"/>
 </div>
 
 <div class="text-center text-2xl mt-4 mb-8">
@@ -271,14 +361,16 @@
 
 <!-- Markov -->
 
-<div class="relative">
+<div class="relative bg-black">
 
     <div class="absolute text-5xl z-40 text-white bottom-4 lg:bottom-12 w-full bg-black rounded-2xl px-4 py-2 bg-opacity-50">
         <div class="text-left ml-[10%] text-xl md:text-2xl lg:text-5xl">
             Markov Chains in Animal Communication
         </div>
     </div>
-    <img alt="a dense forest" src={forest} class="relative w-full max-h-[600px] object-cover"/>
+    <img alt="a dense forest" src={markovDiagram} class="relative w-full max-h-[600px] object-contain"/>
+    <img alt="a bird singing" src={bird} class="absolute w-24 opacity-80 aspect-square left-[49%] top-2/4 -translate-x-2/4 -translate-y-2/4"/>
+
 </div>
 
 <div class="h-48 text-center text-2xl mt-4 mb-8">
